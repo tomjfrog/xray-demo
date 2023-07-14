37cb42dd9b700b0e65503a25ed480a3898f61402
tomjfrog <tomj@jfrog.com>
2023-07-14 15:25:26 -0500

    Initial Commit

---
+++ src/test/java/com/tomjfrog/xraydemo/XrayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomjfrog.xraydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XrayDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
